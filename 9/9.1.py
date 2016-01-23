b63dc14c5162e4a716c93663898226cc928aeb3d
MSB <mat94b@gmail.com>
2016-01-23 14:55:39 +0100

    Dodanie zabeczpieczen

--- 9/9.1.py
+++ 9/9.1.py
@@ -8,43 +8,50 @@ class Node:
 		return str(self.data)
 
 	@staticmethod
-	def remove_head(node):
-		if node is None:
-			raise Exception("pusta lista")
-		if (node.data) is None:
-			raise Exception("pusta lista")
-		if (node.next) is None:
-			node.data = None
-			node.next = None
-		else:
-			helperA = node.next
-			helper = node.next.next
-			node.data = helperA
-			node.next = helper
-			return node
+	def remove_head(node):	
+		try:
+			if node is None:
+				raise Exception("pusta lista")
+			if (node.data) is None:
+				raise Exception("pusta lista")
+			if (node.next) is None:
+				node.data = None
+				node.next = None
+			else:
+				if(node.next is 0):
+					node.data = None
+					node.next = None
+				else:
+					helperA = node.next
+					helper = node.next.next
+					node.data = helperA
+					node.next = helper
+					return node
+		except Exception:
+			print("Lista jest pusta")
 		
 	@staticmethod
 	def remove_tail(node):
-		if node is None:
-			raise Exception("pusta lista")
-		if (node.data) is None:
-			raise Exception("pusta lista")
-		if (node.next) is None:
-			node.data = None
-			node.next = None
-		else:
-			lastOne = 0
-			beforeLastOne = 0
-			while node:
-				beforeLastOne = lastOne
-				lastOne = node
-				node = node.next
-			beforeLastOne.next = 0
-			return node
-
-test = None
-Node.remove_tail(test)
-Node.remove_head(test)
-while test:
-	print test.data
-	test = test.next
+		try:
+			if node is None:
+				raise Exception()
+			if (node.data) is None:
+				raise Exception()
+			if (node.next) is None:
+				node.data = None
+				node.next = None
+			else:
+				if(node.next is 0):
+					node.data = None
+					node.next = None
+				else:
+					lastOne = 0
+					beforeLastOne = 0
+					while node:
+						beforeLastOne = lastOne
+						lastOne = node
+						node = node.next
+					beforeLastOne.next = 0
+					return node
+		except Exception:
+			print("Lista jest pusta")
