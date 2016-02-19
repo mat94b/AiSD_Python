9fca45c9df3358ca4422b4fde6cea6b96a4630c3
MSB <mat94b@gmail.com>
2016-02-19 16:16:55 +0100

    9.2 zadanie

--- 9/9.2.py
+++ 9/9.2.py
@@ -6,10 +6,24 @@ class Node:
 	def __str__(self):
 		return str(self.data)
 
+	def print_list(node):
+		print("L: ")
+		while node:
+			print(node)
+			node = node.next
+		print()
+
 	@staticmethod
 	def merge(node1, node2):
-		w = node1
-		if w.next is not None:
-			w = w.next
-		w.next = node2
-		return node1
+		if(node1 is None) and (node2 is None):
+			return None
+		elif(node1 is None):
+			node1 = node2
+			return node1
+		head = node1
+		last = None
+		while node1:
+			last = node1
+			node1 = node1.next
+		last.next = node2
+		return Node(head.data, head.next)
